8b6fa7719058a3dd15e3e0edd500b24aec7a47fe
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-24 10:59:19 -0700

    Tell httprouter to use our custom 404/405 handlers

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,10 +10,16 @@ func (app *application) routes() http.Handler {
 	// Initialize new httprouter instance
 	router := httprouter.New()
 
+	// Tell httprouter to use our custom notFoundResponse handler.
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+
+	// Tell httprouter to use our custom methodNotAllowed handler.
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register routes
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
