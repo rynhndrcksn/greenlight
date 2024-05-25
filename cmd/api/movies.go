3d3479065e3c064d7cf858f663053f5803632682
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-25 13:43:22 -0700

    Add ValidateMovie method
    Update codebase to use new ValidateMovie method.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/rynhndrcksn/greenlight/internal/data"
+	"github.com/rynhndrcksn/greenlight/internal/validator"
 )
 
 // createMovieHandler creates a new movie.
@@ -18,9 +19,28 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		Genres  []string     `json:"genres"`
 	}
 
+	// Read the JSON body into the anonymous "input" struct.
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Copy the values from the input struct to a new Movie struct.
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// Initialize a new Validator.
+	v := validator.New()
+
+	// Call the ValidateMovie() function and return a response containing the errors if
+	// any of the checks fail.
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
