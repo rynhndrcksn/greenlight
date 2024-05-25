49c101a67d1b5d8e27d3424595a6b696ef80d955
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-25 10:43:56 -0700

    Update createMovieHandler
    Now accepts JSON!

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -10,7 +10,22 @@ import (
 
 // createMovieHandler creates a new movie.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Create a new movie")
+	// Anonymous struct to hold the data sent to us.
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	// Dump the contents of the input struct in a HTTP response.
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // showMovieHandler shows the details of a single movie.
