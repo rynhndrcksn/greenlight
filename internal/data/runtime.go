b24478b1a4cd24bdb7ce3a9d6358b0bc4bd18ac1
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-25 11:24:33 -0700

    Add UnmarshalJSON method to the Runtime type

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,12 +1,17 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 // Runtime is a custom type to be used in our Movie struct.
 type Runtime int32
+
+// ErrInvalidRuntimeFormat defines a custom error for UnmarshalJSON to return.
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 // MarshalJSON method satisfies the json.Marshaler interface.
 // This means we can use this to customize how "Runtime" in our Movie struct looks.
@@ -14,4 +19,35 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedJSONValue := strconv.Quote(jsonValue)
 	return []byte(quotedJSONValue), nil
+}
+
+// UnmarshalJSON method satisfied the json.Unmarshal interface.
+// This lets us accept a format of "<runtime> mins".
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// Remove the quotes around our string.
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Split the string to isolate the part containing the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Sanity check that the string is in the expected format.
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Parse the string containing the number into an int32.
+	i, err := strconv.ParseInt(parts[0], 10, 32)
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	// Convert the int32 into a Runtime type and assign this to the receiver.
+	// Note that we use the * operator to dereference the receiver (which is a pointer
+	// to the Runtime type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
 }
