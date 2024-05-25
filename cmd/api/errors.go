df3861df92a5bf493f64d72c3aae17d5a3db1540
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-25 10:43:30 -0700

    Add a badRequestResponse method

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -36,16 +36,22 @@ func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Reque
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
 
-// The notFoundResponse() method will be used to send a 404 Not Found status code and
+// notFoundResponse method will be used to send a 404 Not Found status code and
 // JSON response to the client.
 func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
 	message := "the requested resource could not be found"
 	app.errorResponse(w, r, http.StatusNotFound, message)
 }
 
-// The methodNotAllowedResponse() method will be used to send a 405 Method Not Allowed
+// methodNotAllowedResponse method will be used to send a 405 Method Not Allowed
 // status code and JSON response to the client.
 func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
 	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
 	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
+
+// badRequestResponse method will be used to send a 400 Bad Request status code and
+// JSON response to the client.
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
