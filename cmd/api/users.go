dcaa3e3ac63caae363e4503aef61f3ae3fe4156c
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-05-31 15:26:36 -0700

    Refactor how welcome email is sent
    No longer blocks the response from being sent.

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -54,7 +54,7 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	err = app.models.Users.Insert(user)
 	if err != nil {
 		switch {
-		// If we get a ErrDuplicateEmail error, use the v.AddError() method to manually
+		// If we get an ErrDuplicateEmail error, use the v.AddError() method to manually
 		// add a message to the validator instance, and then call our
 		// failedValidationResponse() helper.
 		case errors.Is(err, data.ErrDuplicateEmail):
@@ -66,16 +66,16 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	// Call the Send() method on our Mailer, passing in the user's email address,
-	// name of the template file, and the User struct containing the new user's data.
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	// Use the background helper to execute an anonymous function that sends the welcome email.
+	app.background(func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.Error(err.Error())
+		}
+	})
 
 	// Write a JSON response containing the user data along with a 201 Created status code.
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
