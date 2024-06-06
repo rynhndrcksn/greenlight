e8a9f99dd2815755a559cbe020425e93ee5da947
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-06-05 18:33:34 -0700

    Refactor requireAuthenticatedUser middleware

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -90,3 +90,17 @@ func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter
 	message := "invalid or missing authentication token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+// authenticationRequiredResponse will send a 401 Unauthorized status code and
+// JSON response to the client.
+func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must be authenticated to access this resource"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+// inactiveAccountResponse will send a 403 Forbidden status code and
+// JSON response to the client.
+func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account must be activated to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -184,3 +184,61 @@ func (app *application) authenticate(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+//func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		// Use the contextGetUser() helper that we made to retrieve the user
+//		// information from the request context.
+//		user := app.contextGetUser(r)
+//
+//		// If the user is anonymous, then call the authenticationRequiredResponse() to
+//		// inform the client that they should authenticate before trying again.
+//		if user.IsAnonymous() {
+//			app.authenticationRequiredResponse(w, r)
+//			return
+//		}
+//
+//		// If the user is not activated, use the inactiveAccountResponse() helper to
+//		// inform them that they need to activate their account.
+//		if !user.Activated {
+//			app.inactiveAccountResponse(w, r)
+//			return
+//		}
+//
+//		// Call the next handler in the chain.
+//		next.ServeHTTP(w, r)
+//	})
+//}
+
+// Create a new requireAuthenticatedUser() middleware to check that a user is not anonymous.
+func (app *application) requireAuthenticatedUser(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		if user.IsAnonymous() {
+			app.authenticationRequiredResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+// Checks that a user is both authenticated and activated.
+func (app *application) requireActivatedUser(next http.HandlerFunc) http.HandlerFunc {
+	// Rather than returning this http.HandlerFunc we assign it to the variable fn.
+	fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		user := app.contextGetUser(r)
+
+		// Check that a user is activated.
+		if !user.Activated {
+			app.inactiveAccountResponse(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+
+	// Wrap fn with the requireAuthenticatedUser() middleware before returning it.
+	return app.requireAuthenticatedUser(fn)
+}
