37de63dd75f1d6841c5f00ed9bbc9e66c4150c8b
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-06-06 20:05:19 -0700

    Add cmd/example/cors/preflight/main.go
    Update enableCORS method.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -279,6 +279,9 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 		// Add the "Vary: Origin" header.
 		w.Header().Add("Vary", "Origin")
 
+		// Add the "Vary: Access-Control-Request-Method" header.
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		// Get the value of the request's Origin header.
 		origin := r.Header.Get("Origin")
 
@@ -289,9 +292,21 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 			// If there are no trusted origins, the loop won't be iterated.
 			for i := range app.config.cors.trustedOrigins {
 				if origin == app.config.cors.trustedOrigins[i] {
-					// If there is a match, then set an "Access-Control-Allow-Origin" response
-					// header with the request origin as the value and break out of the loop.
 					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					// Check if the request has the HTTP method OPTIONS and contains the "Access-Control-Request-Method" header.
+					// If it does, then we treat it as a preflight request.
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						// Set the necessary preflight response headers, as discussed previously.
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						// Write the headers along with a 200 OK status and return from
+						// the middleware with no further action.
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+
 					break
 				}
 			}
